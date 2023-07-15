6664a6169c5eb8c815c0db8cb01d18efce56cab1
jpmedinagl <jpmedinagl@gmail.com>
2023-07-15 11:03:46 -0400

    adding calculate GPA method for courses

--- src/main/java/entities/Student.java
+++ src/main/java/entities/Student.java
@@ -2,12 +2,22 @@ package entities;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * A Student that has a unique student id. Has a map of courses that they
+ * have successfully completed with the grade they obtained on that course.
+ */
 public class Student {
-
     private final String studentId;
-    private CGPA cgpa;
     private final Map<Course, Integer> courses;
+    private CGPA cgpa;
 
+    /**
+     * Construct a Student with studentId of ID, as well as their courses
+     * taken with courses.
+     *
+     * @param Id the unique student Id.
+     * @param courses the courses the student has completed with grades.
+     */
     public Student(String Id, Map<Course, Integer> courses) {
         this.studentId = Id;
         this.courses = courses;
@@ -26,17 +36,28 @@ public class Student {
         return this.cgpa;
     }
 
+    /**
+     * Add a new course that this student has successfully completed with their
+     * respective grade.
+     *
+     * @param course the new course the student has completed.
+     * @param gpa the grade that is associated with this course.
+     */
     public void addCourse(Course course, Integer gpa) {
         this.courses.put(course, gpa);
     }
 
-    public void addCourses(List<Course> courses, List<Integer> grades) {
-        for (int j = 0; j < courses.size(); j++) {
-            this.addCourse(courses.get(j), grades.get(j));
+    public int calculateCGPA() {
+        int total = 0;
+        for (int grade : this.courses.values()) {
+            total += grade;
         }
+        return (total / this.courses.size());
     }
 
-//    private CGPA calculateCGPA() {
-//        return CGPA();
+//    public void addCourses(List<Course> courses, List<Integer> grades) {
+//        for (int j = 0; j < courses.size(); j++) {
+//            this.addCourse(courses.get(j), grades.get(j));
+//        }
 //    }
 }

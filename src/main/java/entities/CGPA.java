c6daa3cf75575a93c86ae13e5347d8382eeb5cf0
jpmedinagl <jpmedinagl@gmail.com>
2023-07-15 10:32:26 -0400

    adding CGPA entity - completed

---
+++ src/main/java/entities/CGPA.java
@@ -0,0 +1,18 @@
+package entities;
+
+public class CGPA {
+
+    private int cgpa;
+
+    public CGPA(int cgpa) {
+        this.cgpa = cgpa;
+    }
+
+    public int getCgpa() {
+        return this.cgpa;
+    }
+
+    public void setCgpa(int newCgpa) {
+        this.cgpa = newCgpa;
+    }
+}

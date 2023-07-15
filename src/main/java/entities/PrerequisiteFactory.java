d35177959ed583fb08f4b6a6ee3e16b164f078d4
jpmedinagl <jpmedinagl@gmail.com>
2023-07-15 11:45:25 -0400

    javadoc fix

--- src/main/java/entities/PrerequisiteFactory.java
+++ src/main/java/entities/PrerequisiteFactory.java
@@ -1,12 +1,14 @@
 package entities;
 
 /**
- * Factory for creating new Prerequisites.
+ * Factory for creating Prerequisites.
  */
 public class PrerequisiteFactory {
 
     /**
-     * Builds a new Prerequisite with the default grade.
+     * Builds a new Prerequisite with the default grade. This method is static since
+     * the CourseFactory will call this method to create a prerequisite for the course.
+     *
      * @param coursePreReq the course prerequisite.
      * @return Prerequisite object.
      */
@@ -15,7 +17,9 @@ public class PrerequisiteFactory {
     }
 
     /**
-     * Builds a new Prerequisite with the grade required.
+     * Builds a new Prerequisite with the grade required. This method is static since
+     * the CourseFactory will call this method to create a prerequisite for the course.
+     *
      * @param coursePreReq the course prerequisite.
      * @param grade the grade required.
      * @return Prerequisite object.

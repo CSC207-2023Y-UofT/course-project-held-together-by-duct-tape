29860011592dd04989e4f5317fc47dfa3e0daf8c
jpmedinagl <jpmedinagl@gmail.com>
2023-07-19 10:34:49 -0400

    adding javadocs for interfaces

--- src/main/java/login_student_use_case/LoginSessionDataAccess.java
+++ src/main/java/login_student_use_case/LoginSessionDataAccess.java
@@ -1,5 +1,15 @@
 package login_student_use_case;
 
+/**
+ * Interface that the sessionDbGateway must implement for the LoginInteractor to
+ *  * ensure clean architecture and call on its methods.
+ */
 public interface LoginSessionDataAccess {
+    /**
+     * Save the student's information to the session database such that the student
+     * remains logged in until they decide to log out of the program.
+     *
+     * @param requestModel student username.
+     */
     void save(LoginStudentDbRequestModel requestModel);
 }
--- src/main/java/login_student_use_case/LoginStudentDataAccess.java
+++ src/main/java/login_student_use_case/LoginStudentDataAccess.java
@@ -1,5 +1,15 @@
 package login_student_use_case;
 
+/**
+ * Interface that the studentDbGateway must implement for the LoginStudentInteractor to
+ * ensure clean architecture and call on its methods.
+ */
 public interface LoginStudentDataAccess {
+    /**
+     * Method returns a boolean whether a username exists in the student database.
+     *
+     * @param username that will be checked.
+     * @return boolean that indicates if the username exists.
+     */
     boolean usernameExists(String username);
 }

3b8d9b1a73b287936fc2b5eb923ac312e6e516f0
jpmedinagl <jpmedinagl@gmail.com>
2023-07-21 11:18:38 -0400

    adding student test

--- src/test/java/entities/StudentTest.java
+++ src/test/java/entities/StudentTest.java
@@ -1,7 +1,16 @@
 package entities;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.Test;
+
+import java.util.Map;
+import java.util.HashMap;
 
 class StudentTest {
-
+    @Test
+    public void StudentNoCourses() {
+        Map<String, Integer> courses = new HashMap<String, Integer>();
+        Student student = new Student("JP", courses);
+        Assertions.assertEquals("JP", student.getStudentId());
+    }
 }

f3ae9e2676a6c31e0873f02c3efdee1d9e2aba67
jpmedinagl <jpmedinagl@gmail.com>
2023-07-22 12:13:02 -0400

    fixing bugs in null usernames and equality

--- src/main/java/InterfaceAdaptors/CreateStudentInterfaceAdaptors/CreateStudentScreen.java
+++ src/main/java/InterfaceAdaptors/CreateStudentInterfaceAdaptors/CreateStudentScreen.java
@@ -44,7 +44,7 @@ public class CreateStudentScreen extends JPanel implements ActionListener{
 
 
         if(e.getSource() == create){
-            String student = username.getName();
-            String repeated = repeat.getName();
+            String student = username.getText();
+            String repeated = repeat.getText();
             JOptionPane.showMessageDialog(this, controller.create(student, repeated));}
     }}

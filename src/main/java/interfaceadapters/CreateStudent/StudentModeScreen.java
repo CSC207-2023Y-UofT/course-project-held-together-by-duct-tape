0b48c11b93bd12446b23ec9e66f6db6e99e7a7de
hassa963 <nour.hassan@mail.utoronto.ca>
2023-07-22 23:09:32 -0400

    added a return to main menu button to the student mode screen

--- src/main/java/interfaceadapters/CreateStudent/StudentModeScreen.java
+++ src/main/java/interfaceadapters/CreateStudent/StudentModeScreen.java
@@ -15,11 +15,14 @@ public class StudentModeScreen extends JPanel implements ActionListener{
         title.setAlignmentX(Component.CENTER_ALIGNMENT);
         JButton login = new JButton("Login");
         JButton create = new JButton("Create New User");
+        JButton back = new JButton("Return to Main Menu");
         JPanel buttons = new JPanel();
         buttons.add(login);
         buttons.add(create);
+        buttons.add(back);
         create.addActionListener(this);
         login.addActionListener(this);
+        back.addActionListener(this);
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         this.add(title);
         this.add(buttons);
@@ -27,7 +30,6 @@ public class StudentModeScreen extends JPanel implements ActionListener{
     @Override
     public void actionPerformed(ActionEvent e) {
         if( e.getActionCommand().equals("Login")){ card.show(screens, "login");}
-        if(e.getActionCommand().equals("Create New User")){ card.show(screens, "createStudent");
-
-    }
-}}
+        if(e.getActionCommand().equals("Create New User")){ card.show(screens, "createStudent");}
+        if(e.getActionCommand().equals("Return to Main Menu")){card.show(screens, "main");
+}}}

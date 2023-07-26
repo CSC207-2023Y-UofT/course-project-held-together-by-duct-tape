4032dad0a7093bc67b1da7ef86540d6ee73d5fb0
hassa963 <nour.hassan@mail.utoronto.ca>
2023-07-26 11:14:31 -0400

    documentation for CreateStudentCourseDataAccess.java

--- src/main/java/usecases/CreateStudentUsecase/CreateStudentCourseDataAccess.java
+++ src/main/java/usecases/CreateStudentUsecase/CreateStudentCourseDataAccess.java
@@ -1,6 +1,8 @@
 package usecases.CreateStudentUsecase;
 
 import java.util.*;
-
+/**This interface accesses the necessary information that the CreateStudentInteractor
+ *  would need from the course database to create a new user*/
 public interface CreateStudentCourseDataAccess {
+    /**This method retrieves a list of all the course Ids that are currently stored in the Course database*/
     List<String> getCourseIDs();}

b18429db0aa2536d37f59f856ac9126869e66a68
Fermi <91365935+FXXFERMI@users.noreply.github.com>
2023-07-26 21:14:29 -0400

    Delete CourseDataAccess.java

--- CourseDataAccess.java
+++
@@ -1,8 +0,0 @@
-package usecases.InstructorCreateCoursesUseCase;
-import entities.*;
-
-
-public interface CourseDataAccess {
-    boolean courseExists(String courseId);
-    void saveCourse(Course course);
-}

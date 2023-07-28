94a8e38c6c4fa776da7fe66ff634b0b77fc53c29
jpmedinagl <jpmedinagl@gmail.com>
2023-07-28 15:52:49 -0400

    adding driver for database

---
+++ src/main/java/frameworksdrivers/DatabaseDriver.java
@@ -0,0 +1,26 @@
+package frameworksdrivers;
+
+public class DatabaseDriver {
+    private final StudentDbGateway studentDbGateway;
+    private final SessionDbGateway sessionDbGateway;
+    private final CourseDbGateway courseDbGateway;
+
+    public DatabaseDriver() {
+        DbConnection dbConnection = new DbConnection();
+        studentDbGateway = new StudentDbGateway(dbConnection);
+        sessionDbGateway = new SessionDbGateway(dbConnection);
+        courseDbGateway = new CourseDbGateway(dbConnection);
+    }
+
+    public StudentDbGateway getStudentDbGateway() {
+        return studentDbGateway;
+    }
+
+    public SessionDbGateway getSessionDbGateway() {
+        return sessionDbGateway;
+    }
+
+    public CourseDbGateway getCourseDbGateway() {
+        return courseDbGateway;
+    }
+}

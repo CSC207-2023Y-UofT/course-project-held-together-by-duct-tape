29c7433b3df150b298563b11c44f4c220520963c
jpmedinagl <jpmedinagl@gmail.com>
2023-07-28 09:30:52 -0400

    adding complete interactor test

--- src/test/java/usecases/LoginStudentUseCase/LoginStudentInteractorTest.java
+++ src/test/java/usecases/LoginStudentUseCase/LoginStudentInteractorTest.java
@@ -1,7 +1,122 @@
 package usecases.LoginStudentUseCase;
 
-import static org.junit.jupiter.api.Assertions.*;
+import frameworksdrivers.SessionDbGatewayMock;
+import frameworksdrivers.StudentDbGatewayMock;
+import interfaceadapters.LoginStudentInterfaceAdapters.LoginStudentPresenter;
+import interfaceadapters.LoginStudentInterfaceAdapters.LoginUserFailed;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 
 class LoginStudentInteractorTest {
+    private LoginStudentInteractor loginInteractor;
+
+    @BeforeEach
+    public void init() {
+        StudentDbGatewayMock studentDbGateway = new StudentDbGatewayMock();
+        SessionDbGatewayMock sessionDbGateway = new SessionDbGatewayMock();
+        LoginStudentPresenter loginPresenter = new LoginStudentPresenter();
+        loginInteractor = new LoginStudentInteractor(studentDbGateway, sessionDbGateway, loginPresenter);
+    }
+
+    /**
+     * Test empty usernames throw an exception with Empty usernames invalid messsage.
+     */
+    @Test
+    public void testEmptyUsername() {
+        String username = "";
+        String password = "abc";
+        LoginStudentRequestModel requestModel = new LoginStudentRequestModel(username, password);
+
+        // Empty usernames should throw a LoginUserFailed Exception
+        try {
+            loginInteractor.login(requestModel);
+            Assertions.fail("This should not happen.");
+        } catch (LoginUserFailed e) {
+            Assertions.assertEquals(e.getMessage(), "Empty username is invalid.");
+        }
+    }
+
+    /**
+     * Test usernames that don't exist throw an Exception with Username doesn't exist message.
+     */
+    @Test
+    public void testUsernameNotExist() {
+        String username = "jpmg";
+        String password = "abc";
+        LoginStudentRequestModel requestModel = new LoginStudentRequestModel(username, password);
+
+        // Usernames that don't exist should throw a LoginUserFailed Exception
+        try {
+            loginInteractor.login(requestModel);
+            Assertions.fail("This should not happen.");
+        } catch (LoginUserFailed e) {
+            Assertions.assertEquals(e.getMessage(), "Username does not exist.");
+        }
+    }
+
+    /**
+     * Test empty passwords must throw an exception with a Password incorrect message.
+     */
+    @Test
+    public void testEmptyPassword() {
+        String username = "jpmedina";
+        String password = "";
+        LoginStudentRequestModel requestModel = new LoginStudentRequestModel(username, password);
+
+        // Empty passwords should throw a LoginUserFailed Exception
+        try {
+            loginInteractor.login(requestModel);
+            Assertions.fail("This should not happen.");
+        } catch (LoginUserFailed e) {
+            Assertions.assertEquals(e.getMessage(), "Password is incorrect");
+        }
+    }
+
+    /**
+     * Test usernames that exist in database but wrong passwords throw an Exception with a Password incorrect message.
+     */
+    @Test
+    public void testRightUsernameWrongPassword() {
+        String username = "jpmedina";
+        String password = "jpp";
+        LoginStudentRequestModel requestModel = new LoginStudentRequestModel(username, password);
+
+        // Wrong passwords should throw a LoginUserFailed Exception
+        try {
+            loginInteractor.login(requestModel);
+            Assertions.fail("This should not happen.");
+        } catch (LoginUserFailed e) {
+            Assertions.assertEquals(e.getMessage(), "Password is incorrect");
+        }
+    }
+
+    /**
+     * Test that the response model returns the username if the username and password are correct.
+     */
+    @Test
+    public void testRightUsernamePassword() {
+        String username = "jpmedina";
+        String password = "gmpj";
+        LoginStudentRequestModel requestModel = new LoginStudentRequestModel(username, password);
+
+        LoginStudentResponseModel responseModel = loginInteractor.login(requestModel);
+        Assertions.assertEquals(responseModel.getUsername(), requestModel.getUsername());
+    }
+
+    /**
+     * Test that other users that exist in database are able to log in.
+     */
+    @Test
+    public void testMoreCorrectUsernamePassword() {
+        String username = "adelina";
+        String password = "anileda";
+        LoginStudentRequestModel requestModel = new LoginStudentRequestModel(username, password);
+
+        LoginStudentResponseModel responseModel = loginInteractor.login(requestModel);
+        Assertions.assertEquals(responseModel.getUsername(), requestModel.getUsername());
+    }
 
 }

52a5ab6bd9c9f53b71f4a8abf5a22a022cb5fce1
jpmedinagl <jpmedinagl@gmail.com>
2023-07-29 17:27:46 -0400

    javadocs for the drivers

--- src/main/java/frameworksdrivers/DatabaseDriver.java
+++ src/main/java/frameworksdrivers/DatabaseDriver.java
@@ -4,16 +4,27 @@ import frameworksdrivers.Gateways.CourseDbGateway;
 import frameworksdrivers.Gateways.SessionDbGateway;
 import frameworksdrivers.Gateways.StudentDbGateway;
 
+/**
+ * Driver that connects to the databases and creates the gateways. Each gateway has a reference to the connection so
+ * that they are all able to communicate with the database.
+ */
 public class DatabaseDriver implements Driver {
     private final StudentDbGateway studentDbGateway;
     private final SessionDbGateway sessionDbGateway;
     private final CourseDbGateway courseDbGateway;
 
+    /**
+     * Creates a new DbConnection (Database Connection) and creates the various gateways needed.
+     */
     public DatabaseDriver() {
         DbConnection dbConnection = new DbConnection();
         studentDbGateway = new StudentDbGateway(dbConnection);
         sessionDbGateway = new SessionDbGateway(dbConnection);
         courseDbGateway = new CourseDbGateway(dbConnection);
+
+        // Deletes previous sessions that might've not deleted if exited the program wrong.
+        sessionDbGateway.deleteStudentSession();
+        sessionDbGateway.deleteCourseSession();
     }
 
     public StudentDbGateway getStudentDbGateway() {
--- src/main/java/frameworksdrivers/Driver.java
+++ src/main/java/frameworksdrivers/Driver.java
@@ -4,8 +4,12 @@ import frameworksdrivers.Gateways.CourseDbGateway;
 import frameworksdrivers.Gateways.SessionDbGateway;
 import frameworksdrivers.Gateways.StudentDbGateway;
 
+/**
+ * Driver that connects to the databases and creates the gateways. Each gateway has a reference to the connection so
+ * that they are all able to communicate with the database.
+ */
 public interface Driver {
-    public StudentDbGateway getStudentDbGateway();
-    public SessionDbGateway getSessionDbGateway();
-    public CourseDbGateway getCourseDbGateway();
+    StudentDbGateway getStudentDbGateway();
+    SessionDbGateway getSessionDbGateway();
+    CourseDbGateway getCourseDbGateway();
 }
